630f3275d69039be0d437e0862c604203f0c7a12
dr079 <dr079@hdm-stuttgart.de>
2020-09-30 23:27:06 +0200

    Setup Project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app dark>
+    <v-app-bar-nav-icon> </v-app-bar-nav-icon>
+
+    <div @click="$vuetify.goTo('#landing')">
+      <span class="font-weight-bold">Data</span>
+      <span class="font-weight-light">Work</span>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn text @click="$vuetify.goTo('#team')">
+      <v-icon color="blue" small left>mdi-account-group</v-icon>
+      <span class="grey--text text--lighten-1">Team</span>
+    </v-btn>
+
+    <v-btn text @click="$vuetify.goTo('#product')">
+      <v-icon left color="blue" small>mdi-laptop-chromebook</v-icon>
+      <span class="grey--text text--lighten-1">Produkt</span>
+    </v-btn>
+
+    <v-btn text @click="$vuetify.goTo('#service')">
+      <v-icon left color="blue" small>mdi-offer</v-icon>
+      <span class="grey--text text--lighten-1">Service</span>
+    </v-btn>
+
+    <Contact />
+  </v-app-bar>
+</template>
+
+
+<script>
+import Contact from "../components/Contact";
+
+export default {
+  components: { Contact },
+};
+</script>
+
+<style scoped></style>
